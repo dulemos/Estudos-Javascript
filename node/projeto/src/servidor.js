4cd3a969b6edb7a7f436bf3d7e99bb0a238dddce
dulemos <32346249+dulemos@users.noreply.github.com>
2018-08-21 20:56:25 -0300

    node

--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -1,10 +1,40 @@
 const porta = 3003
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const bancoDeDados = require('./bancoDeDados')
+
+app.use(bodyParser.urlencoded({ extended: true }))//transforma o corpo da requissição para um objeto 
 
 
 app.get('/produtos', (req, res) => {
-	res.send({nome: 'Notebook', preco: 123.45}) // converte para json
+	res.send(bancoDeDados.getProdutos()) // converte para json
+})
+
+app.get('/produtos/:id', (req, res) =>{
+	res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) // JSON
+})
+
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) // JSON
+})
+
+app.delete('/produtos/:id', (req, res) => {
+	const produto = bancoDeDados.excluirProduto(req.params.id)
+	res.send(produto) //JSON
 })
 
 app.listen(porta, () => {
